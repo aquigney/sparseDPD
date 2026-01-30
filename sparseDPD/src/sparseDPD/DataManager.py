ebaa89164f77b59bc9d3ed3d3083af49c2d0a218
aquigney <aidan.quigney@ucdconnect.ie>
2026-01-30 14:57:02 +0000

    importing OpenDPD datasets

--- sparseDPD/src/sparseDPD/DataManager.py
+++ sparseDPD/src/sparseDPD/DataManager.py
@@ -2,13 +2,36 @@
 import numpy as np
 import scipy
 from .Dataset import Dataset
+import pandas as pd
 
 class DataManager:
-    def __init__(self, filepath, num_training_points, num_validaiton_points, num_test_points):
+    def __init__(self, num_training_points, num_validaiton_points, num_test_points, filepath=None,
+                 openDPD_test_input_file=None, openDPD_test_output_file=None, openDPD_training_input_file=None, openDPD_training_output_file=None, openDPD_validation_input_file=None, openDPD_validation_output_file=None):
         """Breaks full data set into section"""
 
-        self.filepath = filepath
-        self.input_data, self.output_data = self.read_file()
+        if openDPD_test_input_file and openDPD_test_output_file and openDPD_training_input_file and openDPD_training_output_file and openDPD_validation_input_file and openDPD_validation_output_file:
+            # Create the input and output vectors, the files are all csv files and contain to columns I and Q
+            train_input_df = pd.read_csv(openDPD_training_input_file)
+            train_output_df = pd.read_csv(openDPD_training_output_file)
+            val_input_df = pd.read_csv(openDPD_validation_input_file)
+            val_output_df = pd.read_csv(openDPD_validation_output_file)
+            test_input_df = pd.read_csv(openDPD_test_input_file)
+            test_output_df = pd.read_csv(openDPD_test_output_file)
+
+            # Create complex arrays for all datasets
+            train_input =   self._iq_to_complex(train_input_df)
+            train_output =  self._iq_to_complex(train_output_df)
+            val_input =     self._iq_to_complex(val_input_df)
+            val_output =    self._iq_to_complex(val_output_df)
+            test_input =    self._iq_to_complex(test_input_df)
+            test_output =   self._iq_to_complex(test_output_df)
+            self.input_data = np.concatenate([train_input, val_input, test_input]) 
+            self.output_data = np.concatenate([train_output, val_output, test_output])  
+        elif filepath:
+            self.filepath = filepath
+            self.input_data, self.output_data = self.read_file()
+        else:
+            raise ValueError("Either filepath or all OpenDPD file paths must be provided.")
 
 
         self.num_training_points = num_training_points
@@ -21,6 +44,10 @@ class DataManager:
         self.training_dataset = self.get_training_data()
         self.validation_dataset = self.get_validation_data()
         self.test_dataset = self.get_test_data()
+
+    def _iq_to_complex(self, df):
+        """Convert DataFrame with I and Q columns to complex numpy array"""
+        return (df['I'].values + 1j * df['Q'].values).astype(np.complex128)
 
     def get_training_data(self):
         self.valid_index = self.num_training_points
