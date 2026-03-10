d2ba61f35d298884fb9e42d5cc5fb5b32cc8c268
Aidan Quigney <aidan.quigney@ucdconnect.ie>
2026-03-10 12:41:27 +0000

    show NMSE during training

--- sparseDPD/src/sparseDPD/NeuralNetwork.py
+++ sparseDPD/src/sparseDPD/NeuralNetwork.py
@@ -103,7 +103,7 @@ class NeuralNetwork:
             
             if (epoch + 1) % 10 == 0:
                 current_lr = optimizer.param_groups[0]['lr']
-                print(f"Epoch {epoch + 1:3d}/{num_epochs}  Loss={train_loss:.4e}  Valid Loss={valid_loss:.4e}  LR={current_lr:.2e}")
+                print(f"Epoch {epoch + 1:3d}/{num_epochs}  Loss={train_loss:.4e}  Valid Loss={valid_loss:.4e}  LR={current_lr:.2e}  NMSE={self.calculate_forward_nmse(validation_dataset):.4f} dB")
         
         # Load best model
         self.nn_model.load_state_dict(best_model_state)
